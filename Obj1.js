918b370873e999131e396725bfdf1bfd7568d522
Swati Sangwan <swatisangwan17@gmail.com>
2021-11-26 21:56:24 +0530

    Add files via upload

---
+++ Obj1.js
@@ -0,0 +1,20 @@
+var Person1= {
+    FirstName : "Swati",
+    LastName : "Sangwan",
+    OfficeAddress : "AB Road(Rewari)",
+};
+console.log("1. Name = "+Person1.FirstName +" "+Person1.LastName+", Office Address = "+Person1.OfficeAddress);
+
+var Person2= {
+    FirstName : "Sakshi",
+    LastName : "Sangwan",
+    OfficeAddress : "XY Road(Hisar)",
+};
+console.log("2. Name = "+Person2.FirstName +" "+Person2.LastName+", Office Address = "+Person2.OfficeAddress);
+
+var Person3= {
+    FirstName : "Khushi",
+    LastName : "Sangwan",
+    OfficeAddress : "MN Road(Bhiwani)",
+};
+console.log("3. Name = "+Person3.FirstName +" "+Person3.LastName+", Office Address = "+Person3.OfficeAddress);
